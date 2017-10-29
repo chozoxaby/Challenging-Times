ef838736b5a28ad05e0a73db7bdf4621ec815209
chozoxaby@gmail.com <jcwllms@gmail.com>
2017-10-29 21:23:08 +0000

    Added a route/action/view to display the entire collection of objects from the Zelda table.

--- app/controllers/zeldas_controller.rb
+++ app/controllers/zeldas_controller.rb
@@ -1,2 +1,9 @@
 class ZeldasController < ApplicationController
+  def index
+    @zeldas = Zelda.order(:character)
+  end
+  
+  def show
+    @zelda = Zelda.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   
-  get 'home', to: 'pages#home',  as: 'home'
-  get 'about_us', to: 'pages#about', as: 'about'
+  get 'home',            to: 'pages#home',    as: 'home'
+  get 'about_us',        to: 'pages#about',   as: 'about'
   get 'lizards/:number', to: 'pages#lizards', as: 'lizards',  id: /\d+/
+  get 'zeldas',          to: 'zeldas#index',  as: 'zeldas'
+  get 'zeldas/:id',      to: 'zeldas#show',   as: 'zelda',    id: /\d+/
 end
