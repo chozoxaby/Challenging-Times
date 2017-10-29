83542e61bea43a5c9683006e6b6e326880d49ed5
chozoxaby@gmail.com <jcwllms@gmail.com>
2017-10-29 20:55:04 +0000

    Defined a route for the lizard action that includes a number parameter.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   
   get 'home', to: 'pages#home',  as: 'home'
   get 'about_us', to: 'pages#about', as: 'about'
+  get 'lizards/:number', to: 'pages#lizards', as: 'lizards',  id: /\d+/
 end
